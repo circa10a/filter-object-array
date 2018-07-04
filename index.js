5f70dd43da3d90de68fc928f59807d6c83ee41f9
circa10a <caleblemoine@gmail.comS>
2018-07-04 14:40:37 -0500

    fix useless if, bump version, moves to 100% coverage

--- index.js
+++ index.js
@@ -11,22 +11,20 @@ const _ = require('underscore');
  * @returns {Promise.<array, Error>} Promise that returns array of filtered objects if resolved or error if rejected // eslint-disable-line
  */
 
-module.exports = function filterObjectArray(arr, objFilter, params) {
+module.exports = function filterObjectArray(arr, objFilter, params = {}) {
   return new Promise((resolve, reject) => {
-    if (params) {
       // Convert all values data types as string
-      if (params.ignoreDataType) {
-        const newArr = arr.map(item => _.mapObject(item, val => val.toString()));
-        // Ensure data/json brought in to only have strings for values
-        const modifiedFilters = _.mapObject(objFilter, val => val.toString());
-        // Filter
-        const results = _.filter(newArr, modifiedFilters);
-        // Ensure results were found
-        if (results.length === 0) {
-          reject(new Error('No matching objects found.'));
-        } else {
-          resolve(results);
-        }
+    if (params.ignoreDataType) {
+      const newArr = arr.map(item => _.mapObject(item, val => val.toString()));
+      // Ensure data/json brought in to only have strings for values
+      const modifiedFilters = _.mapObject(objFilter, val => val.toString());
+      // Filter
+      const results = _.filter(newArr, modifiedFilters);
+      // Ensure results were found
+      if (results.length === 0) {
+        reject(new Error('No matching objects found.'));
+      } else {
+        resolve(results);
       }
     // Match data types
     } else {
